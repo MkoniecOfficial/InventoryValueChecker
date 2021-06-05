877cc30115b25759dbc3fd1ef440e234665dcb40
mkoniec <michalkoniec2004@gmail.com>
2021-06-05 21:58:25 +0200

    sieadahsdasd

---
+++ app/src/main/java/com/example/inventoryvaluechecker/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.inventoryvaluechecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val etSteamID = findViewById<TextView>(R.id.SteamIDinput)
+
+        val submit = findViewById<Button>(R.id.confirm)
+
+        val csgobackpack = ("http://csgobackpack.net/api/GetInventoryValue/?id="+etSteamID)
+
+
+
+
+
+
+        if (submit != null && etSteamID != null) {
+            submit.setOnClickListener {
+
+
+            }
+        }
+
+
+    }
+}
